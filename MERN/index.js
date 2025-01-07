37bf6ad268bd5f06441457f2fb25f71aeca65367
KAVIYARASU  M <155162886+KAVIYARASUCCE027@users.noreply.github.com>
2025-01-07 13:55:50 +0530

    Add files via upload
    DAY -2

--- MERN/index.js
+++ MERN/index.js
@@ -79,18 +79,18 @@ var min = 0;
 var hr = 0;
 
 
-setInterval(() => {
-    sec++;
-    if (sec === 60) {
-        sec = 0;
-        min++;
-        if (min === 60) {
-            min = 0;
-            hr++;
-        }
-    }
-    console.log(`Time: ${hr}h ${min}m ${sec}s`);
-}, 1000);
+//setInterval(() => {
+ //   sec++;
+   // if (sec === 60) {
+     ///   sec = 0;
+        //min++;
+        //if (min === 60) {
+          //  min = 0;
+           // hr++;
+        //}
+    //}
+    //console.log(`Time: ${hr}h ${min}m ${sec}s`);
+//}, 1000);
 
 
 setTimeout(() => {
@@ -152,3 +152,165 @@ function fun(){
     console.log("hai js");
 }
 
+let str = "Hello, World!";0
+let start = str.substring(0, 7);
+let end = str.substring(12);    
+let replaced = start + "Universe" + end;
+console.log(replaced);
+
+let stri = "apple banana apple orange";
+let replaceds = str.split("apple").join("grape");
+console.log(replaceds); 
+
+let string = "apple banana apple orange";
+let replaceda = str.replace("apple", "grape");
+console.log(replaceda); 
+
+
+
+
+var t = 8;
+const promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+    if (t === 8) {
+        resolve("on time");
+    } else {
+        reject("late");
+    }
+    },2000)
+});
+
+promise
+    .then((msg) => {
+        console.log(msg);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+
+    function D() {
+        return new Promise((resolve, reject) => {
+            fetch("https://jsonplaceholder.typicode.com/todos/1") 
+                .then((response) => response.json()) 
+                .then((data) => resolve(data))
+                .catch((error) => reject(error));
+        });
+    }
+    
+    D()
+        .then((data) => console.log(data))
+        .catch((error) => console.log("wrong"));
+    
+
+const func=(a,b)=>a+b
+console.log(func(10,20))// does not need return for this function 
+
+
+const pr1 = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("loading...");
+            resolve("data loaded");
+        }, 1000); 
+    });
+};
+
+const pr2 = (prevresult) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("validating...");
+            resolve(prevresult + " | permission validated");
+        }, 1000); 
+    });
+};
+
+const pr3 = (prevresult) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("accessing dashboard...");
+            resolve(prevresult + " | dashboard accessed");
+        }, 1000); 
+    });
+};
+
+pr1()
+.then((result1) => pr2(result1))
+.then((result2) => pr3(result2))
+.then((finalResult) => console.log(finalResult))
+.catch((error) => console.log("Error"));
+
+pr1.call([pr1(),pr2(),pr3()])
+.then(()=>console.log("completed"))
+.catch(()=>console.log("error"));
+
+
+///
+ 
+
+const order = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Order placed...");
+            resolve({ orderId: 123, amount: 50 }); 
+        }, 1000);
+    });
+};
+
+const placeOrder = (orderDetails) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Processing order...");
+            resolve(orderDetails);
+        }, 1000);
+    });
+};
+
+const processPayment = (orderDetails) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Processing payment...");
+            if (orderDetails.amount <= 100) {
+                resolve("Payment successful");
+            } else {
+                reject("Payment error: Amount exceeds limit");
+            }
+        }, 1000);
+    });
+};
+
+const sendConfirmation = (paymentResult) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("Sending confirmation...");
+            resolve("Order confirmed: " + paymentResult);
+        }, 1000);
+    });
+};
+
+order()
+    .then((orderDetails) => placeOrder(orderDetails))
+    .then((orderDetails) => processPayment(orderDetails))
+    .then((paymentResult) => sendConfirmation(paymentResult))
+    .then((confirmationMessage) => console.log(confirmationMessage))
+    .catch((error) => console.log(error));
+
+
+    const orderf =async()=>{
+        try{
+            const data =await placeOrder(total);
+            await processPayment(data);
+            await sendConfirmation();
+            console.log("home page ");
+
+        }
+        catch(error){
+            console.log(error);
+
+        }
+
+    };
+    orderf();
+
+
+    
